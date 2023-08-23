776be07d5058d374779b73e33abfb2b71c4f77e4
AlonsHub <65736848+AlonsHub@users.noreply.github.com>
2023-08-23 03:14:14 +0300

    controller basic done

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,17 +10,41 @@ public class PlayerController : MonoBehaviour
     //
     [SerializeField]
     CharacterController cc;
+    [SerializeField]
+    float walkSpeed;
 
-
+    Vector3 _inputVector;
     // Start is called before the first frame update
     void Awake()
     {
-        
+        if (!cc)
+        {
+            cc = GetComponent<CharacterController>(); //this actually must exist, since it is a required component
+        }    
     }
 
     // Update is called once per frame
     void Update()
     {
+        _inputVector = Vector3.zero;
+
+        _inputVector = Input.GetAxis("Vertical") * transform.forward + Input.GetAxis("Horizontal") * transform.right;
         
+        if (_inputVector.magnitude > 1)
+            _inputVector.Normalize();
+
+        //temp!
+        _inputVector *= walkSpeed;
+
+        if (!cc.isGrounded)
+            _inputVector += Physics.gravity;
+
+        _inputVector *= Time.deltaTime;
+        Move();
+    }
+
+    void Move()
+    {
+        cc.Move(_inputVector);
     }
 }
