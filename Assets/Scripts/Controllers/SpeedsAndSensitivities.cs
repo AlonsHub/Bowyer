57ffc4cf875d4dca269a49b76fcba656c2aa0d71
Alon <alon@cloverbite.com>
2023-10-09 18:42:32 +0300

    Unified rotator and some stats

---
+++ Assets/Scripts/Controllers/SpeedsAndSensitivities.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SpeedsAndSensitivities 
+{
+    //Settings Stats: (user)
+
+    //Base Stats: (gamedesign)
+
+    //Affectors?
+}
